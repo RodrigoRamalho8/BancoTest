457ddd707ce42d97f36cba4fae30777a078d312e
Rodrigo Garcia Ramalho <rodrigogarcia.ramalho@gmail.com>
2024-08-21 15:14:07 -0400

    Finalizando a classe de serviço "Cadastra cliente"

--- BancoTest.Servicos/Cliente/CadastraCliente.cs
+++ BancoTest.Servicos/Cliente/CadastraCliente.cs
@@ -6,22 +6,25 @@ using System.Text;
 using System.Threading.Tasks;
 using BancoTest.Infra;
 using BancoTest.Servicos.DTOs;
+using BancoTest.Servicos.Pessoas;
 
 namespace BancoTest.Servicos.Cliente
 {
     public class CadastraCliente
     {
-        private readonly ContextoBD _contextoBD = new ContextoBD();
+        private readonly ContextoBD _contextoBD;
         private ObtemPessoaPorCPF _obtemPessoaPorCPF;
-        public CadastraCliente(ObtemPessoaPorCPF obtemPessoaPorCPF)
+        public CadastraCliente(ContextoBD contextoBD, ObtemPessoaPorCPF obtemPessoaPorCPF)
         {
+            _contextoBD = contextoBD;
             _obtemPessoaPorCPF = obtemPessoaPorCPF;
         }
-        public ClienteDTO CadastrarCliente(ObtemPessoaPorCPF obtemPessoaPorCPF, int agencia, string tipoConta, string CPF)
+        public void Cadastrar(ObtemPessoaPorCPF obtemPessoaPorCPF, int agencia, string tipoConta, string CPF)
         {
-            Pessoa pessoa = _obtemPessoaPorCPF.ObterPessoaPorCPF(CPF);
-            var novoCliente = new Cliente(agencia, tipoConta, pessoa);
-            return novoCliente;
+            Pessoa pessoadto = _obtemPessoaPorCPF.ObterPessoaPorCPF(CPF);
+            var novoCliente = new BancoTest.Cliente(agencia, tipoConta, pessoadto);
+            _contextoBD.Clientes.Add(novoCliente);
+            _contextoBD.SaveChanges();
         }
     }
 }
