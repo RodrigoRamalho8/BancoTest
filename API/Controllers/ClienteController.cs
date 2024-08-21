37ea810f8f5e6f1a97227428bf8da148b712d34a
Rodrigo Garcia Ramalho <rodrigogarcia.ramalho@gmail.com>
2024-08-21 15:28:52 -0400

    Ajustando controller do Cliente e criando método Get

--- API/Controllers/ClienteController.cs
+++ API/Controllers/ClienteController.cs
@@ -1,13 +1,36 @@
-﻿//using Microsoft.AspNetCore.Mvc;
+﻿using BancoTest.Infra;
+using BancoTest.Servicos.Cliente;
+using Microsoft.AspNetCore.Mvc;
+using BancoTest.Servicos.DTOs;
 
-//namespace API.Controllers
-//{
-//    public class ClienteController : Controller
-//    {
-//        private ContextoBD
-//        public IActionResult Index()
-//        {
-//            return View();
-//        }
-//    }
-//}
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        private ContextoBD _contextoBD;
+        private CadastraCliente _cadastraCliente;
+
+        public ClienteController(ContextoBD contextoBD, CadastraCliente cadastraCliente)
+        {
+            _contextoBD = contextoBD;
+            _cadastraCliente = cadastraCliente;
+        }
+
+        [HttpGet("ObterTodos")]
+        public IEnumerable<ClienteDTO> Get()
+        {
+            return _contextoBD.Clientes.Select(cliente => new ClienteDTO
+            {
+                Agencia = cliente.Agencia,
+                TipoDeConta = cliente.TipoDeConta,
+                Saldo = cliente.Saldo,
+                Pessoa = cliente.Pessoa,
+            });
+        }
+
+        
+
+    }
+}
